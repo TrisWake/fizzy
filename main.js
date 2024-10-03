2d381612c91c2daef98855c1d62291e50c873f1a
Tristan Wakefield <tristan.wakefield@yahoo.com>
2024-10-03 10:02:10 -0500

    finished assignment

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
 /******************************
  * YOUR CODE GOES RIGHT HERE! *
  ******************************/
+function fizzy(num){
+  if (num % 3 === 0 && num % 5 !== 0){
+    return "Fizz";
+  }
+  if (num % 5 === 0 && num % 3 !== 0){
+    return "Buzz"
+  }
+  if (num % 3 === 0 && num % 5 === 0){
+    return "FizzBuzz"
+  }
+  if (num % 3 !== 0 && num % 5 !== 0){
+    return num
+  }
+}
 
 
 
